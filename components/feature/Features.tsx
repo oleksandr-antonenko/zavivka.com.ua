c3b2bd2da1dbfc3f7f12aeddcdc535d71b4277e2
alexandraru <115844615+alexandraru@users.noreply.github.com>
2024-05-31 16:15:54 +0300

    bugfix: change time of scrolling in feature, add some corrections to styling

--- components/feature/Features.tsx
+++ components/feature/Features.tsx
@@ -22,7 +22,7 @@ const Features = () => {
   }
 
   useEffect(() => {
-    const interval = setInterval(() => {carouselInfiniteScroll()}, 3000);
+    const interval = setInterval(() => {carouselInfiniteScroll()}, 1000);
     return () => clearInterval(interval);
   })
 
