3d1203f408b0f8cd8aafa8ba66b1a01f2c312b08
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2025-06-05 16:04:23 +0300

    feat: add security headers to Next.js configuration
    - Implemented security headers for all routes to enhance application security.
    - Added headers including X-Frame-Options, X-XSS-Protection, X-Content-Type-Options, Referrer-Policy, and Permissions-Policy to mitigate common web vulnerabilities.

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,32 @@ const nextConfig = {
   async headers() {
     return [
       {
+        // Apply these headers to all routes
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()',
+          },
+        ],
+      },
+      {
         source: '/:all*(svg|jpg|jpeg|png|webp)',
         headers: [
           {
