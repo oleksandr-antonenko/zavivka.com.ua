43b5de10a581d17576c27c9ea7618ccb0af1a2d5
Oleksandr Antonenko <antonenko.web.dev@gmail.com>
2025-07-29 15:01:00 +0300

    fix urls

--- components/header/top-header/constants.ts
+++ components/header/top-header/constants.ts
@@ -1,12 +1,16 @@
-export const ukraine = ['укр', 'eng'];
+export const ukraine = ["укр", "eng"];
 export const country = [
-  { value: 'ukraine', label: 'Україна' },
-  { value: 'latvia', label: 'Latvia' },
+  { value: "ukraine", label: "Україна" },
+  // { value: 'latvia', label: 'Latvia' },
 ];
-export const latvia = ['укр', 'eng', 'lv', 'рус'];
+export const latvia = ["укр", "eng"];
 
 export const topLinks = [
-  { id: '1', linkNav: '#contacts', titleNav: 'Навчання' },
-  { id: '2', linkNav: '#contacts', titleNav: 'Інтернет-магазин' },
-  { id: '3', linkNav: '#contacts', titleNav: 'Контакти' },
+  { id: "1", linkNav: "https://curl.school", titleNav: "Навчання" },
+  {
+    id: "2",
+    linkNav: "https://shop.zavivka.com.ua",
+    titleNav: "Інтернет-магазин",
+  },
+  { id: "3", linkNav: "#contacts", titleNav: "Контакти" },
 ];
